a373c390e9433e2c1ba9994cc1527a4679dbeae9
Mabe05 <79180185+Mabe05@users.noreply.github.com>
2021-02-27 11:58:31 -0600

    PROGRAMACION1

---
+++ Factorial de un Número.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Factorial_de_un_Número
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, factorial=1, x=0;
+            Console.WriteLine("INTRODUZCA UN NUMERO");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= numero; i++) 
+            {
+                x = x + 1;
+                factorial = factorial * x;
+
+            }
+
+            Console.WriteLine("El factorial de " + numero + " es " + factorial);
+        }
+    }
+}
