2a1045b0ebc14b9f47014e1cb5a0853297939003
Mabe05 <79180185+Mabe05@users.noreply.github.com>
2021-03-05 22:21:17 -0600

    PROGRAMACION1

---
+++ PARCIAL1/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PARCIALPROGRAFINAL
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string RESULTADOUSUARIO = (resultadoUsuario.Text);
+            RESULTADOUSUARIO = RESULTADOUSUARIO.Remove(RESULTADOUSUARIO.Length - 1);
+           
+
+            int RESULTADONUMERO = Convert.ToInt32(RESULTADOUSUARIO);
+            RESULTADONUMERO = RESULTADONUMERO - 4;
+
+           
+            resultadoNumero.Text=RESULTADONUMERO.ToString();
+
+
+        }
+    }
+}
