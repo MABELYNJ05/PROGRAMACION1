90355285aa51875148593a031204a57d19c913e1
Mabe05 <79180185+Mabe05@users.noreply.github.com>
2021-05-21 19:58:29 -0600

    PROGRAMACION1

---
+++ ProyectoFinal_WPF/CLASES/ClsCargaArchivo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_WPF.CLASES
+{
+    class ClsCargaArchivo
+    {
+        public ClsCargaArchivo(DataTable dato)
+        {
+            string datoS = "DPI,NOMBRE,TELEFONO,HABITACIONES,TIPO_ALQUILER,FECHA,PRECIO\n";
+
+            foreach(DataRow x in dato.Rows)
+            {
+                datoS += ($"{x[0]},{x[1]},{x[2]},{x[3]},{x[4]},{x[5]},{x[6]}\n");
+            }
+
+            File.WriteAllText($@"C:\Users\alumno\Desktop\Nueva carpeta\Consultas.csv", datoS);
+
+
+        }
+    }
+}
---
+++ ProyectoFinal_WPF/MainWindow.xaml.cs
@@ -0,0 +1,289 @@
+﻿using ProyectoFinal_WPF.CLASES;
+using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProyectoFinal_WPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //private string nombreTexBox;
+        //private string DPItexBox;
+        //private string telefonoTexbox;
+        DateTime? fecha ;
+        DateTime? Fecha1;
+        DateTime? Fecha2;
+        string nombre;
+        string telefono;
+        string dpi;
+
+
+        public string cantidadMesAño;
+        public string[] alquilarx { get; set; }
+        public string[] habitaciones { get; set; }
+        public string[] num = new string[5] { "1", "2", "3", "4", "5" };
+        public int opcionHabitaciones;
+        public string opcionAlquilarx;
+        public string nombreCons;
+        public int CanHabitacionesCons = 0;
+        public string tipoAlq;
+        string nombreModificar;
+        string opcionModificar;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializerCombobox();
+        }
+
+        private void InitializerCombobox()
+        {
+            alquilarx = new string[] { "Mes", "Año" };
+            habitaciones = new string[] { num[0], num[1], num[2], num[3], num[4] };
+            DataContext = this;
+        }
+
+        private void ButtonGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            nombre = TexBoxNombre.Text;
+            dpi = TexBoxDPI.Text;
+            telefono = TexBoxTelefono.Text;
+            ClsConexion my = new ClsConexion();
+            DataTable dt = my.consultaTablaDirecta(" insert into dbprogra.tb_proyectof values ("+dpi+",'"+nombre+"',"+telefono+","+opcionHabitaciones+",'"+opcionAlquilarx+"','"+fecha.Value.ToString("yyyy/MM/dd")+"',"+cantidadMesAño+");");            
+            TexBoxNombre.Clear();
+            TexBoxDPI.Clear();
+            TexBoxTelefono.Clear();
+            
+
+        }
+
+        private void ButtonSeleccionar_Click(object sender, RoutedEventArgs e)
+        {
+            fecha = SelecFecha.SelectedDate;
+        }
+        
+
+
+        private void Chebox001_Checked(object sender, RoutedEventArgs e)
+        {
+            if (Chebox001.IsChecked == true)
+            {
+                Label001.Content = "001 RENTADO";
+            }
+            else
+            {
+                Label001.Content = "001 DISPONIBLE";
+            }
+
+        }
+
+        private void Chebox002_Checked(object sender, RoutedEventArgs e)
+        {
+            if (Chebox002.IsChecked == true)
+            {
+                Label002.Content = "002 RENTADO";
+            }
+            else
+            {
+                Label002.Content = "002 DISPONIBLE";
+            }
+
+        }
+
+        private void Chebox003_Checked(object sender, RoutedEventArgs e)
+        {
+            if (Chebox003.IsChecked == true)
+            {
+                Label003.Content = "003 RENTADO";
+            }
+            else
+            {
+                Label003.Content = "003 DISPONIBLE";
+            }
+
+        }
+
+        private void Chebox004_Checked(object sender, RoutedEventArgs e)
+        {
+            if (Chebox004.IsChecked == true)
+            {
+                Label004.Content = "004 RENTADO";
+            }
+            else
+            {
+                Label004.Content = "004 DISPONIBLE";
+            }
+        }
+
+        private void Chebox005_Checked(object sender, RoutedEventArgs e)
+        {
+            if (Chebox005.IsChecked == true)
+            {
+                Label005.Content = "005 RENTADO";
+            }
+            else
+            {
+                Label005.Content = "005 DISPONIBLE";
+            }
+        }
+               
+
+        private void ButtonSeleccionar1_Click(object sender, RoutedEventArgs e)
+        {
+            Fecha1 = DatePicker1.SelectedDate;
+        }
+
+        private void ButtonSeleccionar2_Click(object sender, RoutedEventArgs e)
+        {
+            Fecha2 = DatePicker2.SelectedDate;
+        }
+
+        private void ButtonDatosMostrar_Click(object sender, RoutedEventArgs e)
+        {
+            ClsConexion cn = new ClsConexion();
+            DataTable dt = cn.consultaTablaDirecta("SELECT* FROM dbprogra.tb_proyectof where Fecha between '" + Fecha1.Value.ToString("yyyy/MM/dd") + "' and '" + Fecha2.Value.ToString("yyyy/MM/dd") + "'");
+            ClsCargaArchivo ar = new ClsCargaArchivo(dt);
+
+            string datoS = "";
+
+            foreach (DataRow x in dt.Rows)
+            {
+                datoS += ($"{x[0]},{x[1]},{x[2]},{x[3]},{x[4]},{x[5]},{x[6]}\n");
+            }
+
+            ListDatos.ItemsSource = datoS;
+
+        }
+
+        private void ButtonBsxNombre_Click(object sender, RoutedEventArgs e)
+        {
+            string nombre = TexBoxNombreCons.Text;
+            ClsConexion cn = new ClsConexion();
+            DataTable dt = cn.consultaTablaDirecta("SELECT * FROM dbprogra.tb_proyectof where UPPER(Nombre) like ('%"+nombre+"%')");
+            ClsCargaArchivo ar = new ClsCargaArchivo(dt);
+
+            string datoS = "";
+
+            foreach (DataRow x in dt.Rows)
+            {
+                datoS = ($"{x[0]},{x[1]},{x[2]},{x[3]},{x[4]},{x[5]},{x[6]}\n");
+            }
+
+            TexBoxNombreCons.Clear();
+
+            ListDatos.ItemsSource = datoS;
+
+
+        }
+
+        private void ButtonBsxHabit_Click(object sender, RoutedEventArgs e)
+        {
+            int CanHabit = Convert.ToInt32(ComboBoxHabitCons.SelectedItem);
+            ClsConexion cn = new ClsConexion();
+            DataTable dt = cn.consultaTablaDirecta("SELECT * FROM dbprogra.tb_proyectof where Habitaciones = "+CanHabit+"");
+
+            string datoS = "";
+
+            foreach (DataRow x in dt.Rows)
+            {
+                datoS = ($"{x[0]},{x[1]},{x[2]},{x[3]},{x[4]},{x[5]},{x[6]}\n");
+            }
+
+            ListDatos.ItemsSource = datoS;
+
+
+        }
+
+        private void ButtonBsxAlq_Click(object sender, RoutedEventArgs e)
+        {
+            string tipoAlq = Convert.ToString(ComboConsAql.SelectedItem);
+            ClsConexion cn = new ClsConexion();
+            DataTable dt = cn.consultaTablaDirecta("SELECT * FROM dbprogra.tb_proyectof where Tipo_Alquiler ='"+tipoAlq+"'");
+
+            string datoS = "";
+
+            foreach (DataRow x in dt.Rows)
+            {
+                datoS += ($"{x[0]},{x[1]},{x[2]},{x[3]},{x[4]},{x[5]},{x[6]}\n");
+            }
+
+            ListDatos.ItemsSource = datoS;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ClsConexion cn = new ClsConexion();
+            opcionModificar = Convert.ToString(ModificarAql.SelectedItem);
+            DataTable dt = cn.consultaTablaDirecta("UPDATE dbprogra.tb_proyectof SET Tipo_Alquiler='" + opcionModificar + "' WHERE UPPER (Nombre) like('%"+nombreModificar+"%')");
+            ModificarNom.Clear();
+            ButtonAlqActual.Content = "ALQUILER ACTUAL";
+        }
+
+        private void ButtonAlqActual_Click(object sender, RoutedEventArgs e)
+        {
+            nombreModificar = ModificarNom.Text;
+            ClsConexion cn = new ClsConexion();
+            DataTable nm = cn.consultaTablaDirecta("SELECT Tipo_Alquiler FROM dbprogra.tb_proyectof WHERE UPPER (Nombre) like('%" + nombreModificar + "%')");
+            ButtonAlqActual.Content = "" + nm;
+            
+
+        }
+
+        private void ButtonSelecAlq_Click(object sender, RoutedEventArgs e)
+        {
+            opcionAlquilarx = Convert.ToString(ComboboxAlquilarx .SelectedItem);
+        }
+
+        private void ButtonSelecHab_Click(object sender, RoutedEventArgs e)
+        {
+            cantidadMesAño = cantidadAlq.Text;
+            string opcion = Convert.ToString(ComboboxHabitaciones.SelectedItem);
+            opcionHabitaciones = Convert.ToInt32(opcion);
+
+            switch (opcionHabitaciones)
+            {
+                case 1:
+                    num[4] = "---------";
+                    break;
+
+                case 2:
+                    num[3] = "---------";
+                    break;
+
+                case 3:
+                    num[2] = "---------";
+                    break;
+
+                case 4:
+                    num[1] = "---------";
+                    break;
+
+                case 5:
+                    MessageBox.Show("Estimado usuario, usted a rentado todas nuestras habitaciones, por lo cual obtendrá un 5% de descuento");
+                    Label001.Content = "RENTADO";
+                    Label002.Content = "RENTADO";
+                    Label003.Content = "RENTADO";
+                    Label004.Content = "RENTADO";
+                    Label005.Content = "RENTADO";
+                    break;
+
+            }
+
+        }
+    }
+}
