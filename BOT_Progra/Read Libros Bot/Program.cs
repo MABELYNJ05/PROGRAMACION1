99a060fc67fada10ca0d6ba4afbdc83b37284137
Mabe05 <79367405+MABELYNJ05@users.noreply.github.com>
2021-06-07 11:41:52 -0600

    PROGRAMACION1

---
+++ BOT_Progra/Read Libros Bot/Program.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+
+using Read_Libros_Bot.Funciones;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+
+
+
+namespace Read_Libros_Bot
+{
+    class Program
+    {
+        // public static async Task Main()
+        public static async Task Main(string[] args)
+        {
+             await new Escuchar().IniciarTelegram();
+
+           
+
+        }
+
+    }
+}
